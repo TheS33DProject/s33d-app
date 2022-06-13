f8e0792ba979e4d5c7bf152a5391119818a70e1c
ksharma027 <kapils@scalacoders.com>
2022-06-13 13:12:19 +0530

    fix: Swiper Carousel integrated

--- src/views/Home/components/Carousal/index.tsx
+++ src/views/Home/components/Carousal/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Flex, Heading, Text } from '@pancakeswap/uikit'
 import styled from 'styled-components'
-import Carousel from 'nuka-carousel'
+import { Swiper, SwiperSlide } from 'swiper/react' // import Carousel from 'nuka-carousel'
+import 'swiper/swiper.min.css'
+import 'swiper/components/effect-coverflow/effect-coverflow.min.css'
+import 'swiper/components/pagination/pagination.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+import SwiperCore, { EffectCoverflow, Pagination, Autoplay } from 'swiper/core'
 import useTheme from 'hooks/useTheme'
 import './carousel.css'
+
+SwiperCore.use([EffectCoverflow, Pagination, Autoplay])
 
 const DesktopContent = styled.div``
 
@@ -36,9 +43,110 @@ function HomeCarousel() {
     fontSize: '18px',
   }
 
+  // const navigation = {
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // }
+
   return (
     <>
-      <Carousel
+      <Swiper
+        style={{ width: '100vw' }}
+        autoplay={{
+          delay: 5000,
+        }}
+        centeredSlides
+        spaceBetween={50}
+        pagination={{
+          clickable: true,
+        }}
+        loop
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <Flex mb="30px" alignItems="center" flexDirection="column" justifyContent="center">
+            <Heading
+              style={isDark ? { ...darkThemeStyle } : { ...headingFontStyle }}
+              scale="xl"
+              mb="24px"
+              textAlign="center"
+            >
+              The S33D Project
+            </Heading>
+            <DesktopContent>
+              <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+                This is a 100% community owned sustainability initiative. Together, we’re reimagining how we use our
+                resources to conserve our planet for generations to come.
+              </Text>
+              <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+                Finance for growers
+              </Text>
+              <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+                Powered by blockchain, accelerated by DeFi.
+              </Text>
+            </DesktopContent>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Flex alignItems="center" flexDirection="column" justifyContent="center">
+            <Heading
+              style={isDark ? { ...darkThemeStyle } : { ...headingFontStyle }}
+              scale="xl"
+              mb="24px"
+              textAlign="center"
+            >
+              Plant A S33D
+            </Heading>
+            <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+              All life happens with planting a seed.
+            </Text>
+            <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+              Our initiative is easy. Tell S33D farmers what you want and the community responds with fair offers for
+              locally grown and harvested foods at specific time frames.
+            </Text>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Flex alignItems="center" flexDirection="column" justifyContent="center">
+            <Heading
+              style={isDark ? { ...darkThemeStyle } : { ...headingFontStyle }}
+              scale="xl"
+              mb="24px"
+              textAlign="center"
+            >
+              Grow Food
+            </Heading>
+            <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+              Get funded to start your passion for growing loved fruits of labor.
+            </Text>
+            <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+              Mark your presence, establish your credentials, and thrive while growing what you love, knowing they will
+              be enjoyed locally.
+            </Text>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Flex alignItems="center" flexDirection="column" justifyContent="center">
+            <Heading
+              style={isDark ? { ...darkThemeStyle } : { ...headingFontStyle }}
+              scale="xl"
+              mb="24px"
+              textAlign="center"
+            >
+              Fund Causes
+            </Heading>
+            <Text textAlign="center" p="20px" style={isDark ? { ...contentFontStyleDark } : { ...contentFontStyle }}>
+              Mobilise funds to reach the causes that matter to you. Let the planet be a little better to live in when
+              funds go to where it matters.
+            </Text>
+          </Flex>
+        </SwiperSlide>
+      </Swiper>
+      {/* <Carousel
         animation="fade"
         // wrapAround="true"
         style={{ marginTop: '80px', width: '100vw' }}
@@ -55,7 +163,6 @@ function HomeCarousel() {
           },
           pagingDotsContainerClassName: 'page-dots-container',
         }}
-        // wrapAround
       >
         <Flex mb="30px" alignItems="center" flexDirection="column" justifyContent="center">
           <Heading
@@ -98,7 +205,6 @@ function HomeCarousel() {
               locally grown and harvested foods at specific time frames.
             </Text>
           </Flex>
-          {/* <img src="/assets/images/carousel/imageHd1.jpg" alt="1" /> */}
         </>
         <>
           <Flex alignItems="center" flexDirection="column" justifyContent="center">
@@ -135,7 +241,7 @@ function HomeCarousel() {
             </Text>
           </Flex>
         </>
-      </Carousel>
+      </Carousel> */}
     </>
   )
 }
